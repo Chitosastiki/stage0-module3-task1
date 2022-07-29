c10975908c180e72a41fd6db00b99b9212ce50f2
Chitosastiki <110228508+Chitosastiki@users.noreply.github.com>
2022-07-29 14:55:37 +0400

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.snippet("java" + "is"  +"smth");
+        System.out.println("java" + "is"  +"smth");
     }
 }
